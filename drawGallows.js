feb09d5bde784a5af8242edead8582613a2fb4ae
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-13 14:40:40 +0100

    draws gallows on canvas

--- drawGallows.js
+++ drawGallows.js
@@ -1,4 +1,16 @@
 function drawGallows() {
     var canvas = document.querySelector('#canvas');
     var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(20, 180);
+    ctx.lineTo(40, 160);
+    ctx.lineTo(60, 180);
+    ctx.moveTo(40, 160);
+    ctx.lineTo(40, 20);
+    ctx.lineTo(120, 20);
+    ctx.lineTo(120, 50);
+    ctx.moveTo(40, 40);
+    ctx.lineTo(60, 20)
+    ctx.stroke();
 }
+drawGallows();
