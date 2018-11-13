86b32d0fee0dc7f9c1932df997afbbb6a1446979
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-13 22:04:41 +0100

    adds random password (visible) on start and restart with clearing previous password

--- manageGame.js
+++ manageGame.js
@@ -1,6 +1,9 @@
 function startGame() {
     const playButton = document.querySelector('.play-button')
     playButton.addEventListener('click', () => {
+        document.querySelectorAll('span').forEach(function (node) {
+            node.remove();
+        });
         play();
         playButton.innerHTML = 'Restart';
     });
@@ -15,7 +18,13 @@ function getPassword() {
     fetch('https://hangman-25d5c.firebaseio.com/nouns.json')
         .then(response => response.json())
         .then(passwords => {
-             var password = passwords[Math.floor(Math.random() * passwords.length)];
-             console.log(password);
-         })
+            var password = passwords[Math.floor(Math.random() * passwords.length)];
+            console.log(password);
+            for (let i = 0; i < password.length; i++) {
+                const letterBox = document.createElement('span');
+                const passwordBox = document.querySelector('.password');
+                letterBox.innerHTML = password[i];
+                passwordBox.appendChild(letterBox);
+            }
+        })
 }
