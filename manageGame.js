640b0c83bbaae945e1c107fd7f1a85b56c934298
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-14 21:16:27 +0100

    adds valid letter input functionality and restructures code managing input handling

--- manageGame.js
+++ manageGame.js
@@ -11,26 +11,72 @@ function startGame() {
 startGame();
 
 function play() {
-    getPassword(setPassword);
+    getPassword(manageGame);
 }
 
-function setPassword(password) {
+function manageGame(password) {
+    displayEmptyPassword(password);
+    handleInput(password);
+}
+
+function displayEmptyPassword(password) {
     for (let i = 0; i < password.length; i++) {
         const letterBox = document.createElement('span');
         const passwordBox = document.querySelector('.password');
         letterBox.innerHTML = '';
         passwordBox.appendChild(letterBox);
     }
+    console.log(password);
+}
+
+function handleInput(password) {
+    const form = document.querySelector(".letter-form")
+    const input = document.querySelector(".letter-input")
+    const reg = /^[a-z]+$/i;
+
+    form.addEventListener('submit', (event) => {
+        if (input.value === '' || input.value.length > 1 || !reg.test(input.value)) {
+            event.preventDefault();
+            input.value = '';
+            input.placeholder = "One english letter please";
+            input.classList.add('warning');
+        }
+        else {
+            console.log('inside else')
+            event.preventDefault();
+            handleValidInput(password, input.value);
+            input.value = '';
+            input.placeholder = "Your letter here";
+            input.classList.remove('warning');
+        }
+    })
+}
+
+function handleValidInput(password, letter) {
+    console.log('inside handleValidInput')
+    console.log(password)
+    console.log(letter)
+    for (let i = 0 ; i < password.length ; i++) {
+            if (letter === password[i]) {
+                let indexToShow = document.querySelector(`span:nth-child(${i+1})`)
+                console.log(indexToShow);
+                indexToShow.innerHTML = password[i];
+            }
+        }
 }
 
 function getPassword(callback) {
     fetch('https://hangman-25d5c.firebaseio.com/nouns.json')
         .then(response => response.json())
         .then(passwords => {
-            var password = passwords[Math.floor(Math.random() * passwords.length)];
+            let password = passwords[Math.floor(Math.random() * passwords.length)];
             callback(password);
         })
 }
 
+// var lettersLeft = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// var lettersLeftCount = lettersLeft.length;
+// var lettersUsed = [];
+// var lettersUsedCount = lettersUsed.length;
 
-
+//TODO: prevent double letter use 
