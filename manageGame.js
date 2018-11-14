7b6eade8b8a5c260ac3659305004464584261481
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-14 10:47:39 +0100

    rewrites fetching passwords via fetchAPI to callbacks

--- manageGame.js
+++ manageGame.js
@@ -11,20 +11,28 @@ function startGame() {
 startGame();
 
 function play() {
-    getPassword();
+    getPassword(setPassword);
 }
 
-function getPassword() {
+function setPassword(password) {
+    console.log(password);
+    for (let i = 0; i < password.length; i++) {
+        const letterBox = document.createElement('span');
+        const passwordBox = document.querySelector('.password');
+        letterBox.innerHTML = password[i];
+        passwordBox.appendChild(letterBox);
+    }
+}
+
+function getPassword(callback) {
     fetch('https://hangman-25d5c.firebaseio.com/nouns.json')
         .then(response => response.json())
         .then(passwords => {
             var password = passwords[Math.floor(Math.random() * passwords.length)];
-            console.log(password);
-            for (let i = 0; i < password.length; i++) {
-                const letterBox = document.createElement('span');
-                const passwordBox = document.querySelector('.password');
-                letterBox.innerHTML = '';
-                passwordBox.appendChild(letterBox);
-            }
+            callback(password);
         })
 }
+
+
+
+
